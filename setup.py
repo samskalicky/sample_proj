f1910a1c9364ccb17c7865772e7974018d0f97e3
Sam Skalicky <samskalicky@gmail.com>
2019-12-03 21:10:13 -0800

    working pip wheel build scripts

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
+
 setuptools.setup(
         name="jenkinsProj",
         version="1.0",
         description="simulating jenkins ci",
-
+        package_dir={'':'python'},
+        packages=setuptools.find_packages("python")
         )
